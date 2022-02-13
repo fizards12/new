8110f64a8030dacd41f9bb9bcdc508d42e4c6156
fizards12 <mahmoudsameh734@gmail.com>
2022-02-13 17:31:09 +0200

    page not finished

---
+++ tst/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const myName = "Mahmoud";
+const hello = (userName) => `hello, ${userName}`;
+console.log(hello(myName));
+const MyNum = '15' % 2;
+console.log(MyNum);
---
+++ tst/dist/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var myName = "Mahmoud";
+var hello = function (userName) { return "hello, " + userName; };
+console.log(hello(myName));
+var MyNum = '15' % 2;
+console.log(MyNum);
